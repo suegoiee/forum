46b91f319a7ed1156e431c257e5efbc6f82b2711
yoyo <kfp87198@gmail.com>
2019-04-03 12:04:48 +0800

    0403_changed_4

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -4,7 +4,7 @@ return [
 
     'banned' => '此帳戶已被禁用!',
     'fields' => '發生錯誤，請確認下面欄位!',
-    'unconfirmed' => '<a href="'.route('email.send_confirmation').'" class="alert-link">請先確認您的郵件地址!</a>',
+    'unconfirmed' => '<a href="'.route('email.send_confirmation').'" class="alert-link" style="color: #ef3474;">請先至您的信箱驗證信件!</a>',
     'github_invalid_state' => '執行超過時間，請再嘗試一次!',
     'github_account_too_young' => '您的帳戶需在兩周後才能再次註冊!',
 
