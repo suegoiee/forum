4ba807404b3f211658e5b0c6592a31f5a1d6ec04
yoyo <kfp87198@gmail.com>
2019-04-08 15:42:25 +0800

    0408_changed_4

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base')
 
 @section('body')
-    <div class="container" style="min-height: calc(100vh - 100px);">
+    <div class="container" style="min-height: calc(100vh - 100px); padding-left: 0px; padding-right: 0px;">
         @include('layouts._alerts')
 
         @yield('content')
