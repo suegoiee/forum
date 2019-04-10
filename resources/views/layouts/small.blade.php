2f82944d09c96c1194d78000022f7f502e1c1c60
shouwda <shouwda@uanalyze.com.tw>
2019-04-10 04:24:56 +0000

    adjust login alert message position

--- resources/views/layouts/small.blade.php
+++ resources/views/layouts/small.blade.php
@@ -4,14 +4,13 @@
     <div class="container" style="min-height: calc(100vh - 150px);">
         <div class="row loginForm">
             <div class="col-md-4 col-md-offset-4">
-                @include('layouts._alerts')
-
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <img src="images/logo_verticle_colour.svg" style="width: 80%; margin: 5% auto 8%;">
                         <br/>
                         {{ $title }}
                     </div>
+                    @include('layouts._alerts')
                     <div class="panel-body">
                         @yield('small-content')
                     </div>
