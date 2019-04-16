a745bd5e220a560fc3e2e26122ac681daa948cab
Joshhuang <joshhuang@uanalyze.com.tw>
2019-04-16 14:39:45 +0800

    email confirm test

--- app/Jobs/SendEmailConfirmation.php
+++ app/Jobs/SendEmailConfirmation.php
@@ -41,13 +41,13 @@ class SendEmailConfirmation extends Notification
      * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function handle($notifiable)
+    public function handle()
     {
         return (new MailMessage)
             ->subject('請認證您的優分析論壇帳號')
             ->from(env('APP_EMAIL','no-reply@localhost'),env('APP_SYSTEM_NAME','Service'))
             ->line('我們需要認證您的信箱來開通您的帳號，請點選下方按鈕完成認證:')
-            ->action('認證信箱', route('email.confirm').'?email='.urlencode($user->emailAddress()).'&code='. $user->confirmationCode())
+            ->action('認證信箱', route('email.confirm').'?email='.urlencode($this->user->emailAddress()).'&code='. $this->user->confirmationCode())
             ->line('再次感謝您對'."<a href=".route('home').">優分析論壇</a>".'的支持');
     }
 }
