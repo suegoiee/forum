b000f812240fd3b51383cd0665b0f1d76309ce97
Joshhuang <joshhuang@uanalyze.com.tw>
2019-04-16 17:25:13 +0800

    google tag manager test

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,6 +12,14 @@
     gtag('config', 'UA-138435332-1');
     </script>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5BB6FQM');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- Global site tag (gtag.js) - Google Analytics >
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112922258-4"></script>
     <script>
@@ -22,11 +30,11 @@
     </script-->
 
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <!--script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-W82JCL7');</script>
+    })(window,document,'script','dataLayer','GTM-W82JCL7');</script-->
     <!-- End Google Tag Manager -->
 
     <!-- Facebook Pixel Code -->
@@ -78,7 +86,12 @@
 </head>
 <body class="{{ $bodyClass ?? '' }}">
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W82JCL7"
+<!--noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W82JCL7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript-->
+<!-- End Google Tag Manager (noscript) -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BB6FQM"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
