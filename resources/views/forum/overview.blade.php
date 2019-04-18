6a7a8eb3226fd4fc27318b01014ef4da30469b8d
yoyo <kfp87198@gmail.com>
2019-04-18 16:31:22 +0800

    0418_forum.tags.changetype

--- resources/views/forum/overview.blade.php
+++ resources/views/forum/overview.blade.php
@@ -18,7 +18,7 @@
 
             <div class="dropdown formDrop">
                 <a class="dropdownTitle" role="button" data-target="#titleTable" data-toggle="collapse" aria-expanded="false">
-                    所有主題
+                    所有分類 <i class="far fa-plus-square"></i><i class="far fa-minus-square"></i>
                 </a>
                 <div class="dropdownMenu forumTitle collapse" aria-expanded="false" id="titleTable">
                     <a class="dropdownItem {{ active('forum*', ! isset($activeTag) || $activeTag === null) }}" href="{{ route('forum') }}">全部<a>
