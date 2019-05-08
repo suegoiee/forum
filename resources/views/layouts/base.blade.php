4f1fb5852491ad5de2268ec5719473bd86d40acc
yoyo <kfp87198@gmail.com>
2019-05-08 17:23:10 +0800

    0508_renew

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -16,9 +16,8 @@
     document.write(_LoadingHtml);
     //监听加载状态改变
     document.onreadystatechange = completeLoading;
-    //加载状态为complete时移除loading效果
     function completeLoading() {
-        if (document.readyState == "complete") {
+        if (document.readyState == "interactive") {
             var loadingMask = document.getElementById('loadingDiv');
             loadingMask.parentNode.removeChild(loadingMask);
         }
