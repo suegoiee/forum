d5d30c88500d71867f5b239e67b102e63ac606a4
Joshhuang <joshhuang@uanalyze.com.tw>
2019-05-29 14:20:10 +0800

    info type value

--- resources/views/freebie/Report.blade.php
+++ resources/views/freebie/Report.blade.php
@@ -15,7 +15,7 @@
     </div>
     <div class="container" id="CanvasBaseMap" style="margin-left: 0;padding-left: 0;">
         <div class="container" style="padding-left: 0; padding-right: 0;">
-            <h1 style="margin-top: 15px; margin-bottom: 0; padding-left: 15px;" id="info_type">{{$PageSubtitle}}</h1>
+            <h1 style="margin-top: 15px; margin-bottom: 0; padding-left: 15px;" id="info_type" value="{{$PageSubtitle}}">{{$PageSubtitle}}</h1>
             <h2 style="margin-top: 10px; margin-bottom: 10px; padding-left: 15px;" id="stock_title">{{$stock_code . ' - ' . $stock_name}}</h2>
             <div class="container" id="Outer" style="margin-left: 0;"></div>
         </div>
