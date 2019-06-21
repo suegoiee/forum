04c144b8195ab3b747a779b022931b90204efb91
shouwda <dragon19290@gmail.com>
2019-06-21 09:08:39 +0800

    change shortcut size

--- resources/views/layouts/_favicons.blade.php
+++ resources/views/layouts/_favicons.blade.php
@@ -1,10 +1,10 @@
 <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/favicon-01.png">
 <link id="favicon16" rel="icon" href="/images/favicons/favicon_16.png" type="image/png" size="16x16">
 <link id="favicon24" rel="icon" href="/images/favicons/favicon_24.png" type="image/png" size="24x24">
-<link id="favicon32" rel="shortcut icon" href="/images/favicons/favicon_32.png" type="image/png" size="32x32">
+<link id="favicon32" rel="icon" href="/images/favicons/favicon_32.png" type="image/png" size="32x32">
 <link id="favicon48" rel="icon" href="/images/favicons/favicon_48.png" type="image/png" size="48x48">
 <link id="favicon64" rel="icon" href="/images/favicons/favicon_64.png" type="image/png" size="64x64">
-<link id="favicon256" rel="icon" href="/images/favicons/favicon_256.png" type="image/png" size="256x256">
+<link id="favicon256" rel="shortcut icon" href="/images/favicons/favicon_256.png" type="image/png" size="256x256">
 <link rel="manifest" href="/site.webmanifest">
 <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#2b5797">
