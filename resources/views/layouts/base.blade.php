c3fdddb8d85f4196606bf1632599ba122b51b0e9
yoyo <kfp87198@gmail.com>
2019-06-27 11:28:04 +0800

    0627 canvas fix

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,34 +2,6 @@
 <html lang="{{ App::getLocale() }}">
 <head>
 
-    <!-- loading -->
-    <script>
-    //获取浏览器页面可见高度和宽度
-    var _PageHeight = document.documentElement.clientHeight,
-        _PageWidth = document.documentElement.clientWidth;
-    //计算loading框距离顶部和左部的距离（loading框的宽度为215px，高度为61px）
-    var _LoadingTop = _PageHeight > 61 ? (_PageHeight - 61) / 2 : 0,
-        _LoadingLeft = _PageWidth > 215 ? (_PageWidth - 215) / 2 : 0;
-    //在页面未加载完毕之前显示的loading Html自定义内容
-    var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight + 'px;top:0;background: rgba(0,0,0,0.8);opacity:1;filter:alpha(opacity=80);z-index:10000;"><div style="position: absolute; cursor1: wait; left: ' + _LoadingLeft + 'px; top:' + _LoadingTop + 'px; width: 200px; height: 150px; line-height: 57px; padding-left: 50px; padding-right: 5px; background: url(/images/loading.svg) no-repeat scroll;"></div></div>';
-    
-    /*會造成head 格式錯誤請修正寫法
-    
-    //呈现loading效果
-    document.write(_LoadingHtml);
-    //监听加载状态改变
-    document.onreadystatechange = completeLoading;
-    //加载状态为complete时移除loading效果
-    function completeLoading() {
-        if (document.readyState == "complete") {
-            var loadingMask = document.getElementById('loadingDiv');
-            loadingMask.parentNode.removeChild(loadingMask);
-        }
-    }
-    */
-    </script>
-    <!-- end loading -->
-
     <!-- meta -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -187,7 +159,32 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
+</script>
+
+<!-- html2canvas start -->
+<script type="text/javascript" src="/js/html2canvas.js"></script>
+<script type="text/javascript">
+    function convertById(id) {
+        html2canvas(document.querySelector("#"+id),{
+            canvas: document.querySelector("#myCanvas")
+        }).then(canvas => {
+        });
+    }
+    var canvas = document.getElementById("myCanvas");
+    download_img = function(el) {
+        var image = canvas.toDataURL("image/jpg");
+        el.href = image;
+    }
+    $(document).on("click", ".downloadImg", function(){
+        var clickedId = $(this).next().next().attr('id');
+        convertById(clickedId);
+    });
+
+    $(document).on("click", ".print", function(){
+        convertById('CanvasBaseMap');
+    });
     
+
     /*axios.get('https://cronjob.uanalyze.com.tw/fetch/CompanyInfo/1101')
         .then(function (response) {
             console.log(response);
@@ -195,6 +192,56 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         .catch(function (error) {
         });*/
 </script>
+<!-- html2canvas end -->
+
+<!-- button to position -->
+
+<script>
+
+$('.rightbtnA').click(function(){
+        if(window.screen.availHeight > 900){
+            var height = -200 ;
+            $('html,body').animate({scrollTop:$('#getInfo').offset().top + height},800);
+        }
+        else {
+            var height = -150 ;
+            $('html,body').animate({scrollTop:$('#getInfo').offset().top + height},800);
+        }
+    });
+    $('.rightbtnB').click(function(){
+        if(window.screen.availHeight > 900){
+            var height = -200 ;
+            $('html,body').animate({scrollTop:$('#getNews').offset().top + height},800);
+        }
+        else {
+            var height = -150 ;
+            $('html,body').animate({scrollTop:$('#getNews').offset().top + height},800);
+        }
+    });
+    $('.rightbtnC').click(function(){
+        if(window.screen.availHeight > 900){
+            var height = -200 ;
+            $('html,body').animate({scrollTop:$('#dailyChart').offset().top + height},800);
+        }
+        else {
+            var height = -150 ;
+            $('html,body').animate({scrollTop:$('#dailyChart').offset().top + height},800);
+        }
+    });
+    $('.rightbtnD').click(function(){
+        if(window.screen.availHeight > 900){
+            var height = -200 ;
+            $('html,body').animate({scrollTop:$('#stockChart').offset().top + height},800);
+        }
+        else {
+            var height = -150 ;
+            $('html,body').animate({scrollTop:$('#stockChart').offset().top + height},800);
+        }
+    });
+
+</script>
+
+<!-- button end -->
 
 <!--end of for development-->
 
