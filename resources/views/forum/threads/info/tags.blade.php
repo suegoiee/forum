ef461c55082f1ba056c66a0a6d7fce3c657b3ecc
Joshhuang <joshhuang@uanalyze.com.tw>
2019-07-04 10:43:47 +0800

    console tags

--- resources/views/forum/threads/info/tags.blade.php
+++ resources/views/forum/threads/info/tags.blade.php
@@ -1,4 +1,8 @@
 @if (count($thread->tags()))
+    <script>
+        var masters = @json($thread);
+        console.log(masters);
+    </script>
     <div class="thread-info-tags">
         @foreach ($thread->tags() as $tag)
             <a href="{{ route('forum.tag', $tag->slug()) }}">
