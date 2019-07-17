11e7e03e7b695779c1d1f52ec93b30c37b6c77d2
Joshhuang <joshhuang@uanalyze.com.tw>
2019-07-17 15:47:02 +0800

    migrate data

--- routes/web.php
+++ routes/web.php
@@ -89,6 +89,7 @@ Route::prefix('admin')->name('admin')->namespace('Admin')->group(function () {
     Route::get('/', 'AdminController@index');
     Route::get('/justifyAuthorId', 'AdminController@changeAuthorIdToUaVersion')->name('.justifyAuthorId');
     Route::get('/addCategoeyTypeToCategoryThread', 'AdminController@addCategoeyTypeToCategoryThread')->name('.addCategoeyTypeToCategoryThread');
+    Route::get('/migrateData', 'AdminController@migrateData')->name('.migrateData');
     Route::get('category', 'AdminController@category')->name('.category');
     Route::post('category', 'AdminController@newCategory')->name('.category.create');
     Route::post('newCategoryProduct', 'AdminController@newCategoryProduct')->name('.newCategoryProduct.create');
