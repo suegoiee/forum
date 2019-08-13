abc894b7346e3354ca044564bfa081e170266ff6
Joshhuang <joshhuang@uanalyze.com.tw>
2019-08-13 11:58:47 +0800

    profile image data

--- resources/views/experts/overview.blade.php
+++ resources/views/experts/overview.blade.php
@@ -7,7 +7,7 @@
         @foreach($experts as $expert)
             <div class="card" style="width:25%; position:relative; float:left; margin:4%;">
                 <a href="{{route('experts.profile', $expert->id)}}">
-                    <img class="card-img-top" src="{{$expert->avatar}}" alt="Card image" style="width:100%">
+                    {!!$expert->image!!}
                 </a>
                 <div class="card-body">
                     <h4 class="card-title">{{$expert->expert_name}}</h4>
