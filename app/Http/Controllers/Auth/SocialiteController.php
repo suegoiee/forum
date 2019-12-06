6073c4fb328cf4a1563695a9e483fdfcd52779e6
Joshhuang <joshhuang@uanalyze.com.tw>
2019-12-06 12:27:23 +0800

    google login test

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -7,6 +7,7 @@ use App\User;
 use Socialite;
 use App\Social\GithubUser;
 use App\Jobs\UpdateProfile;
+use Illuminate\Http\Request;
 use App\Jobs\RegisterGoogleUser;
 use App\Jobs\RegisterUAUserConfirmed;
 use App\Http\Controllers\Controller;
@@ -30,9 +31,11 @@ class SocialiteController extends Controller
     /**
      * Obtain the user information from GitHub.
      */
-    public function handleProviderCallback()
+    public function handleProviderCallback(Request $request)
     {
+        dd($request);
         try {
+            //$socialiteUser = Socialite::driver('google')->user();
             $socialiteUser = Socialite::driver('google')->stateless()->user();
         } catch (InvalidStateException $exception) {
             $this->error('errors.github_invalid_state');
@@ -40,7 +43,6 @@ class SocialiteController extends Controller
         }
 
         try {
-            dd($socialiteUser);
             $user = User::findByEmailAddress($socialiteUser->getEmail());
         } catch (ModelNotFoundException $exception) {
             return $this->userNotFound($socialiteUser);
@@ -64,15 +66,90 @@ class SocialiteController extends Controller
 
     private function userNotFound(SocialiteUser $socialiteUser): RedirectResponse
     {
-        $this->dispatchNow(new RegisterGoogleUser($socialiteUser->getName(), $socialiteUser->getEmail(), $socialiteUser->getName(), '', '', $socialiteUser->getId(), 2, 1));
+        /*$this->dispatchNow(new RegisterGoogleUser($socialiteUser->getName(), $socialiteUser->getEmail(), $socialiteUser->getName(), '', '', $socialiteUser->getId(), 2, 1));*/
+        
+        $socialite = SocialiteDB::where('provider', 'google')->where('provider_id', $socialiteUser['id'])->first();
+
+        $socialite_data = [
+            'provider'=>'google',
+            'provider_id'=>$socialiteUser['id'],
+            'name'=>$socialiteUser['name'],
+            'email'=>$socialiteUser['email'],
+            'access_token'=>$access_token,
+        ];
+
+        if($socialite){
+            $user = $socialite->user;
+            $socialite->update([
+                'email'=>$socialite_data['email'],
+                'name'=>$socialite_data['name'],
+                'access_token'=>$socialite_data['access_token']
+            ]);
+            if(!$user->mail_verified_at){
+                User::where('id',$user->id)->update(['mail_verified_at'=>date('Y-m-d H:i:s'),'confirmed'=>1]);
+            }
+            $user->touch();
+            return $this->logined($request, $user, $mobile);
+        }else{
+            
+            $user = User::where('email',$socialite_data['email'])->first();
+            if(!$user){
+                $user = $this->create($request->all());
+                $user->socialite()->create($socialite_data);
+                return $this->registered($request, $user, $mobile);
+            }
+            if(!$user->mail_verified_at){
+                User::where('id',$user->id)->update(['mail_verified_at'=>date('Y-m-d H:i:s'),'confirmed'=>1]);
+            }
+            $user->socialite()->create($socialite_data);
+            return $this->logined($request, $user, $mobile);
+        }
+
         $user = User::findByEmailAddress($socialiteUser->getEmail());
         $this->registered($user);
         Auth::login($user);
         $this->success('歡迎來到優分析');
         return redirect()->route('forum');
     }
-    protected function registered($user)
+
+    protected function registered(Request $request, $user, $mobile)
     {
-        return $this->dispatchNow(new RegisterUAUserConfirmed($user));
+        $this->createProfile($request, $user);
+        $adminToken = $this->clientCredentialsGrantToken($request);
+        event(new UserRegistered($user, $adminToken, $request->input('password'), false));
+        $client = $mobile ? $this->getMobilePasswordGrantClient() : $this->getPasswordGrantClient();
+        $user_token = $user->createToken($client->name);
+        $token = [
+            'token_type'=>'Bearer',
+            'expires_in'=>$user_token->token->expires_at->toDateTimeString(),
+            'access_token'=>$user_token->accessToken,
+            'refresh_token'=>'',
+            'verified'=>$user->mail_verified_at ? 1 : 0,
+            'is_socialite'=>$user->is_socialite,
+            'set_password'=> $user->set_password
+        ];
+        return $this->successResponse($token);
+    }
+    protected function logined(Request $request,$user, $mobile)
+    {
+        $client = $mobile ? $this->getMobilePasswordGrantClient() : $this->getPasswordGrantClient();
+        $user_token = $user->createToken($client->name);
+        $token = [
+            'token_type'=>'Bearer',
+            'expires_in'=>$user_token->token->expires_at->toDateTimeString(),
+            'access_token'=>$user_token->accessToken,
+            'refresh_token'=>'',
+            'verified'=>$user->mail_verified_at ? 1 : 0,
+            'is_socialite'=>$user->is_socialite,
+            'set_password'=> $user->is_socialite !=0 && $user->version == 1 ? 0 : $user->set_password
+        ];
+        $this->updateProfile($request,$user);
+        return $this->successResponse($token);
+    }
+
+    protected function createProfile(Request $request,$user){
+        $store_data = $request->only(['nickname','name','sex','address','birthday']);
+        $profile = $user->profile()->create($store_data);
+        return $profile;
     }
 }
