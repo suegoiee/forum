578ded2c903ef1b871385724c24c5ec6fcc8b658
Joshhuang <joshhuang@uanalyze.com.tw>
2019-12-06 16:17:46 +0800

    google login test

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -25,7 +25,7 @@ class SocialiteController extends Controller
      */
     public function redirectToProvider()
     {
-        return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')->stateless()->redirect();
     }
 
     /**
@@ -35,8 +35,6 @@ class SocialiteController extends Controller
     {
         try {
             //$socialiteUser = Socialite::driver('google')->user();
-            $state = $request->get('state');
-            $request->session()->put('state',$state);
             $socialiteUser = Socialite::driver('google')->stateless()->user();
         } catch (InvalidStateException $exception) {
             $this->error('errors.github_invalid_state');
